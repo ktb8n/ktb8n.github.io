8939016f6a2f98a69f0be70f52e47e144b9eb054
Katie Baeten <katherinebaeten@gmail.com>
2025-04-22 16:26:51 -0500

    updated devgallery descriptions

--- src/components/DevGallery.jsx
+++ src/components/DevGallery.jsx
@@ -2,10 +2,10 @@ import React, { useState } from "react";
 import ArtCard from "./ArtCard";
 import DevDetailModal from "./DevDetailModal";
 import movingEyesImage from "../assets/dev/SofonisbaAnguissola.png";
-import mitBusImage from "../assets/dev/realTimeBus.png";
-import errorToITImage from "../assets/dev/errorToIT1.png";
-import shippingFormImage from "../assets/dev/shippingForm1.png";
-import outlookerImage from "../assets/dev/outlookerImagePrivate.png";
+import mitBusImage from "../assets/dev/RealTimeBus.png";
+import errorToITImage from "../assets/dev/ErrorToIT1.png";
+import shippingFormImage from "../assets/dev/ShippingForm1.png";
+import outlookerImage from "../assets/dev/OutlookerImagePrivate.png";
 import productivityTimerImage from "../assets/dev/ProductivityTimerImage.png";
 export default function DevGallery() {
 	const [activeProject, setActiveProject] = useState(null);
@@ -15,7 +15,7 @@ export default function DevGallery() {
 			id: "dev1",
 			title: "Moving Eyes",
 			image: movingEyesImage,
-			description: "SAMPLE INFORMATION",
+			description: "This project was created to demonstrate the use of CSS and JavaScript to create a moving eyes effect. The eyes follow the mouse cursor, creating an interactive experience. It is in the process of being reformatted as a react component",
 			githubURL: "",
 			tooltip: "scooby doo style css tricks",
 		},
@@ -23,7 +23,7 @@ export default function DevGallery() {
 			id: "dev2",
 			title: "Productivity Timer",
 			image: productivityTimerImage,
-			description: "Time management tool used first in classroom settings and later on tuned to scrum meetings",
+			description: "Timer application to help manage time. It is a work in progress and is being developed as a react component. The timer is designed to help users stay focused and manage their time effectively.",
 			githubURL: "",
 			tooltip: "time management tool",
 		},
@@ -32,7 +32,7 @@ export default function DevGallery() {
 			title: "MIT Bus Routes",
 			image: mitBusImage,
 			description:
-				"This project made use of mapbox-gl api and public data from MIT to track busses between the Harvard and MIT campuses. Using a starter code outline I hard coded coordinate updates. I extended the code to update the busses' positions in real-time.",
+				"This project was created to demonstrate the use of mapbox-gl api and public data from MIT to track busses between the Harvard and MIT campuses. Using a starter code outline I hard coded coordinate updates. I extended the code to update the busses' positions in real-time.",
 			githubURL: "",
 			tooltip: "mapbox-gl api and public data from MIT to track busses",
 		},
@@ -42,7 +42,7 @@ export default function DevGallery() {
 			image: errorToITImage,
 			description: "",
 			githubURL: "",
-			tooltip: "ASP.NET winform application",
+			tooltip: "ASP.NET winform application made to alleviate the number of tickets coming with insufficient information to troubleshoot them.",
 		},
 		{
 			id: "dev5",
@@ -50,7 +50,7 @@ export default function DevGallery() {
 			image: shippingFormImage,
 			description: "Blazor WebAssembly application",
 			githubURL: "",
-			tooltip: "Blazor WebAssembly application",
+			tooltip: "Blazor WebAssembly application developed to help manage and standardize shipping information.",
 		},
 		{
 			id: "dev6",
@@ -58,7 +58,7 @@ export default function DevGallery() {
 			image: outlookerImage,
 			description: "Terminal application to manage Outlook email attachments",
 			githubURL: "",
-			tooltip: "Terminal application to manage Outlook email attachments",
+			tooltip: "Terminal application that automates the process of downloading and organizing email attachments from Outlook. It saves all pdf attachments as an OCR pdf, names it according to our naming convention and saves it in the correct place for accounting.",
 		},
 	];
 
