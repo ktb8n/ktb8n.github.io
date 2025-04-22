49322fe96ea3e018098a39fade8efd3346fa3ed5
Katie Baeten <katherinebaeten@gmail.com>
2025-04-22 12:52:54 -0500

    deployment hot fix

--- vite.config.js
+++ vite.config.js
@@ -5,5 +5,5 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
 	plugins: [react()],
 	assetsInclude: ["**/*.JPG"],
-	base: "/ktb8n.github.io",
+	base: "./ktb8n.github.io",
 });
